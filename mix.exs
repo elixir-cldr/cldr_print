924b5907c37601d4e27fb9aed8943014f9ed9821
Kip Cole <kipcole9@gmail.com>
2019-03-28 04:48:23 -0700

    Bump cldr_numbers version

--- mix.exs
+++ mix.exs
@@ -2,6 +2,7 @@ defmodule Cldr.Print.MixProject do
   use Mix.Project
 
   @version "0.2.0"
+
   def project do
     [
       app: :ex_cldr_print,
@@ -59,7 +60,7 @@ defmodule Cldr.Print.MixProject do
   defp deps do
     [
       {:nimble_parsec, "~> 0.5"},
-      {:ex_cldr_numbers, path: "../cldr_numbers"},
+      {:ex_cldr_numbers, "~> 2.6"},
       {:jason, "~> 1.0"},
       {:ex_doc, "~> 0.19"},
       {:benchee, "~> 0.14", only: [:dev, :test]}
